d3b1d2eacb389bad06888bb9f80806f91fad8cb1
Boonkung <boonkung1123@gmail.com>
2022-08-07 11:44:12 +0700

    add form controller

--- lib/screen/play_screen.dart
+++ lib/screen/play_screen.dart
@@ -9,7 +9,7 @@ class PlayScreen extends StatefulWidget {
 
 class _PlayScreenState extends State<PlayScreen> {
   int number = 0;
-
+  TextEditingController nameController = TextEditingController();
   String name = "God of war";
   void increments() {
     setState(() {
@@ -41,18 +41,41 @@ class _PlayScreenState extends State<PlayScreen> {
           children: [
             Text("Now value is  : ${number}"),
             Text("Name is : ${name}"),
+            Container(
+              margin: EdgeInsets.all(10),
+              width: 250,
+              child: Form(
+                child: Column(
+                  children: [
+                    Text("Name : "),
+                    TextFormField(
+                      controller: nameController,
+                    ),
+                  ],
+                ),
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.purple)),
                   onPressed: increments,
                   child: Icon(Icons.add),
                 ),
                 ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red)),
                   onPressed: decrements,
                   child: Icon(Icons.remove),
                 ),
                 ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green)),
                   onPressed: () => {ChangeName("God of bin")},
                   child: Icon(Icons.edit),
                 ),
